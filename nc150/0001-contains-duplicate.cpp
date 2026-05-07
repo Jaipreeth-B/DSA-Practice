6d997a47141797f36982f6d06e41d17a99486f89
Jai <jaipreeth04@gmail.com>
2026-05-07 06:02:08 +0530

    Initial Practice Roadmap NC150

---
+++ nc150/0001-contains-duplicate.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define FASTIO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+
+// Type Aliases
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vc vector<char>
+#define vvc vector<vector<char>>
+#define vs vector<string>
+#define vvs vector<vector<string>>
+#define vb vector<bool>
+#define vvb vector<vector<bool>>
+
+// Constants
+const int MOD = 1e9 + 7;
+const int MOD2 = 998244353;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const ld EPS = 1e-9;
+const ld PI = acos(-1.0);
+
+// Utility Macros
+#define stf shrink_to_fit
+#define pb push_back
+#define popb pop_back
+#define mp make_pair
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define el '\n'
+
+// Loop Macros
+#define rep(i,a,b) for (auto i=(a); i<(b); i++)
+#define rrep(i,a,b) for (auto i=(a); i>=(b); i--)
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(auto &it : nums)
+        {
+            freq[it]++;
+            if(freq[it] == 2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    FASTIO;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        Solution sol;
+        vi nums = {1,1,5,5,3,2,6,7};
+        bool check = sol.containsDuplicate(nums);
+        check ? cout << "True\n" : cout << "False\n";
+    }
+    return 0;
+}
